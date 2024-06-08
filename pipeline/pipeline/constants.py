306da65e0a6acba1806c994515719ce5a056a770
tricktx <patrick.tx@hotmail.com.br>
2024-06-08 15:23:20 -0300

    finish download and concat data anp

--- pipeline/pipeline/constants.py
+++ pipeline/pipeline/constants.py
@@ -3,10 +3,13 @@ class constants(Enum):
     """
     Enum class that represents a collection of constants used in the pipeline.
     """
+    HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
 
-    INPUT = '/tmp/anp/input/'
+    INPUT = '/home/tricktx/project_pipeline_anp/download_files/'
     
-    OUTPUT = '/tmp/anp/output/'
+    OUTPUT = '/home/tricktx/project_pipeline_anp/output/'
     
     URLS = [
             "https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/shpc/qus/ultimas-4-semanas-glp.csv",
@@ -14,7 +17,7 @@ class constants(Enum):
             "https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/shpc/qus/ultimas-4-semanas-diesel-gnv.csv",
         ]
     
-    path = [
+    PATH = [
         INPUT + 'ultimas-4-semanas-glp.csv',
         INPUT + 'ultimas-4-semanas-gasolina-etanol.csv',
         INPUT + 'ultimas-4-semanas-diesel-gnv.csv'
--- pipeline/pipeline/web_scrapping_anp.py
+++ pipeline/pipeline/web_scrapping_anp.py
@@ -3,11 +3,43 @@ import pandas as pd
 from constants import constants
 import os
 
-def download_files(urls: str, path: str) -> None:
-    os.system(f"mkdir -p {path}")
+def download_files(urls: str) -> None:
+    os.makedirs(constants.INPUT.value, exist_ok=True)
     for url in urls:
-        r = requests.get(url)
-        with open (path, 'wb') as f:
+        # Verify=False is necessary to avoid SSL verification, look:
+        ##  https://requests.readthedocs.io/projects/pt/pt-br/latest/user/advanced.html
+        ##  https://www.cloudflare.com/pt-br/learning/ssl/what-is-an-ssl-certificate/
+        r = requests.get(url, verify=False, headers=constants.HEADERS.value)
+        file_name = url.split('/')[-1]
+        path_file = os.path.join(constants.INPUT.value, file_name)
+        with open(path_file, 'wb') as f:
             f.write(r.content)
-        
-download_files(constants.URLS.value, constants.path.value)
+
+def concat_files() -> pd.DataFrame:
+    download_files(constants.URLS.value)
+    df_total = []
+    for paths in constants.PATH.value:
+        df = pd.read_csv(paths, sep=';', encoding='utf-8')
+        df_total.append(df)
+
+        df = pd.concat(df_total, ignore_index=False)
+
+    return df
+
+def partition_data(df: pd.DataFrame) -> None:
+    for year in df['Data da Coleta'].str[6:10].unique():
+
+
+        for sigla in df['Estado - Sigla'].unique():
+
+            partition = os.makedirs(f'{constants.OUTPUT.value}/{year}/{sigla}', exist_ok=True)
+            df_partition = df[(df['Data da Coleta'].str[6:10].unique()) & (df['Estado - Sigla'] == sigla)]
+            df_partition.drop(columns = ['Data da Coleta', 'Estado - Sigla'], inplace=True)
+            df_partition_path = f'{constants.OUTPUT.value}/{year}/{sigla}/data.csv'
+            df_partition.to_csv(df_partition_path, encoding='utf-8', index=False, na_rep = '')
+
+def main() -> None:
+    df = concat_files()
+    partition_data(df)
+
+main()
