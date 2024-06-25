025aa217864bcc52827cb331e82bb1cc2ab5765b
tricktx <patrick.tx@hotmail.com.br>
2024-06-25 14:55:17 -0300

    create bucket in gcs

--- pipeline/pipeline/upload_gcs.py
+++ pipeline/pipeline/upload_gcs.py
@@ -1,7 +1,24 @@
 import pandas as pd
 import os
 from google.cloud import storage
-from pipeline.pipeline.web_scrapping_anp import main
-def upload_files(bucket_name: str, local_path: str, gcs_path: str) -> None:
+import os
 
+
+# get the variable environment that contains the path to the service account
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/tricktx/project_pipeline_anp/service_account_anp/anp.json"
+
+# define function that creates the bucket
+def create_bucket(bucket_name, storage_class='STANDARD', location='us-central1'): 
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+    bucket.storage_class = storage_class
+   
+    bucket = storage_client.create_bucket(bucket, location=location) 
+    # for dual-location buckets add data_locations=[region_1, region_2]
     
+    return f'Bucket {bucket.name} successfully created.'
+
+create_bucket(bucket_name = 'anp_bucket',
+              storage_class='STANDARD',
+              location='us-central1')
